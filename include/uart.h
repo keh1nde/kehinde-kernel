0e1883eeced25617806f6eaf26b705b5bcbef5da
Kehinde Adeoso <kennolite@gmail.com>
2026-04-01 03:22:34 -0400

    moved functions to header

--- include/uart.h
+++ include/uart.h
@@ -7,3 +7,18 @@ void uart_putc(unsigned char c);
 unsigned char uart_getc();
 
 void uart_puts(const char* str);
+
+static inline void mmio_write(uint32_t reg, uint32_t data)
+{
+	*reinterpret_cast<volatile uint32_t*>(reg) = data;  // or *(volatile uint32_t*)reg = data;
+}
+
+static inline uint32_t mmio_read(uint32_t reg)
+{
+	return *reinterpret_cast<volatile uint32_t*>(reg); // or *(volatile uint32_t*)reg;
+}
+
+static inline void delay(int32_t count) {
+	asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
+						: "=r"(count): [count]"0"(count) : "cc");
+}
--- src/uart/uart.cpp
+++ src/uart/uart.cpp
@@ -2,21 +2,6 @@
 #include <stdint.h>
 #include "uart.h"
 
-static inline void mmio_write(uint32_t reg, uint32_t data)
-{
-	*reinterpret_cast<volatile uint32_t*>(reg) = data;  // or *(volatile uint32_t*)reg = data;
-}
-
-static inline uint32_t mmio_read(uint32_t reg)
-{
-	return *reinterpret_cast<volatile uint32_t*>(reg); // or *(volatile uint32_t*)reg;
-}
-
-static inline void delay(int32_t count) {
-	asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
-						: "=r"(count): [count]"0"(count) : "cc");
-}
-
 enum {
 	GPIO_BASE = 0x3F200000,
 
