0c17d9bf5f2e004e9632ca86cb3f265cbf681752
Kehinde Adeoso <kennolite@gmail.com>
2026-04-03 04:50:39 -0400

    added uart_put_uint test code. moved timer and interrupt initialization.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -11,24 +11,29 @@ extern "C" void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
 	(void) atags;
 
 	uart_init();
-	interrupt_init();
-	timer_init();
 
 	uart_puts("Hello, kernel World!\r\n");
+
+	uart_puts("Today is the ");
+	uart_put_uint(3);
+	uart_puts(" of April. \n");
 
 	uint64_t el;
 	asm volatile("mrs %0, CurrentEL" : "=r"(el));
 	switch (el) {
 		case (4):
-			uart_puts("You are in Exception Level One \n");
+			uart_puts("This is Exception Level One \n");
 			break;
 		case (8):
-			uart_puts("You are in Exception Level Two \n");
+			uart_puts("This is Exception Level Two \n");
 			break;
 		case (12):
-			uart_puts("You are in Exception Level Three \n");
+			uart_puts("This is Exception Level Three \n");
 		default: uart_puts("No EL found \n");
 	}
+
+	interrupt_init();
+	timer_init();
 
 	while (1) {
 		uart_putc(uart_getc());
