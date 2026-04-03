d861b90785e332373f30df75037097f63fe5deb8
Kehinde Adeoso <kennolite@gmail.com>
2026-04-03 04:51:58 -0400

    refactored code. implemented int and hex uart printing.

--- include/uart.h
+++ include/uart.h
@@ -1,13 +1,5 @@
 #pragma once
 
-void uart_init();
-
-void uart_putc(unsigned char c);
-
-unsigned char uart_getc();
-
-void uart_puts(const char* str);
-
 static inline void mmio_write(uint32_t reg, uint32_t data)
 {
 	*reinterpret_cast<volatile uint32_t*>(reg) = data;  // or *(volatile uint32_t*)reg = data;
@@ -22,3 +14,17 @@ static inline void delay(int32_t count) {
 	asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
 						: "=r"(count): [count]"0"(count) : "cc");
 }
+
+void uart_init();
+
+void uart_putc(unsigned char c);
+
+unsigned char uart_getc();
+
+void uart_puts(const char* str);
+
+void uart_put_hex(uint64_t val);
+
+void uart_put_uint(uint64_t val);
+
+void print_helper(char val);
