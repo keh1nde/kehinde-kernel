cf63f7f0b2a0b15013b03e08758f4ea2833f7ce6
Kehinde Adeoso <kennolite@gmail.com>
2026-04-24 22:35:26 -0400

    Declared all mmu methods

--- include/mmu.h
+++ include/mmu.h
@@ -3,7 +3,31 @@
 //
 
 #pragma once
-
-#include <cstdint.h>
 #include <stdint-gcc.h>
+#include <stdint>
 
+
+/**
+ * Configures MAIR_EL1, TCR_EL1, TTBR0_EL1, and TTBR1_EL1
+ * Installs a top-level table
+ * Flush TBLs
+ * Flip SCTLR_EL1.M to 1
+ */
+void mmu_init();
+
+
+/**
+ * Walk and allocate via alloc_frame the L0 to L3 tables, and install a descriptor at the terminal level
+ * Flags encode cacheablilty (Normal vs Device via MAIR index), permissions and access control.
+ */
+void map(uint64_t va, uint64_t phys, uint64_t size, uint64_t flags);
+
+// Clear descriptor, invalidate TLB entry
+void unmap(uint64_t va, uint64_t size);
+
+// Walks tables in software and returns physical addresses (or find faults)
+uint64_t translate(uint64_t virt);
+
+uint64_t* _get_or_alloc_table(uint64_t* table, uint64_t index);
+
+uint64_t extractBits(unsigned int num, int p, int k);
