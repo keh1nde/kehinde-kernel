9779abb78af17b80a147c583115d106258ce99f5
Kehinde Adeoso <kennolite@gmail.com>
2026-04-29 14:38:31 -0400

    Implemented debug code in sync handler

--- src/interrupts/interrupts.cpp
+++ src/interrupts/interrupts.cpp
@@ -67,6 +67,79 @@ enum {
 	IRQ_C0_SOURCE = 0x40000060,
 
 };
+extern "C" void handle_synchronous_interrupts() {
+
+
+	uint64_t esr, far, elr;
+	asm volatile("mrs %0, ESR_EL1" : "=r"(esr));
+	asm volatile("mrs %0, FAR_EL1" : "=r"(far));
+	asm volatile("mrs %0, ELR_EL1" : "=r"(elr)); // PC at faulting instruction
+
+
+	uint64_t ec = (esr >> 26) & 0x3F;
+	uint64_t iss = esr & 0x1FFFFFF;
+	uint64_t dfsc = iss & 0x3F;
+
+	switch (ec) {
+		case 0x20:
+			uart_puts("Instruction Abort from a lower EL.");
+			uart_puts("FAR_EL1: ");
+			uart_put_hex(far);
+			uart_puts("\nDFSC: ");
+			uart_put_hex(dfsc);
+			while (true) {}
+		case 0x21:
+			uart_puts("Instruction Abort from the same EL.\n");
+			uart_puts("There is likely no VA to PA mapping, or you did it wrong.");
+			uart_puts("FAR_EL1: ");
+			uart_put_hex(far);
+			uart_puts("\nDFSC: ");
+			uart_put_hex(dfsc);
+			uart_put_hex(far);
+			while (true) {}
+		case 0x24:
+			uart_puts("Data Abort from a lower EL \n");
+			uart_puts("FAR_EL1: ");
+			uart_put_hex(far);
+			uart_puts("\nDFSC: ");
+			uart_put_hex(dfsc);
+			uart_put_hex(far);
+			while (true) {}
+		case 0x25:
+			uart_puts("Data Abort from the same EL \n");
+			uart_puts("Kernel did a load/store to an unmapped or wrong-perm VA.");
+			uart_puts("FAR_EL1: ");
+			uart_put_hex(far);
+			uart_puts("\nDFSC: ");
+			uart_put_hex(dfsc);
+			while (true) {}
+		case 0x0E:
+			uart_puts("Illegal execution state \n");
+			uart_puts("FAR_EL1: ");
+			uart_put_hex(far);
+			uart_puts("\nDFSC: ");
+			uart_put_hex(dfsc);
+			while (true) {}
+		case 0x15:
+			uart_puts("SVC from AArch64: System call. \n");
+
+	}
+
+	/*
+	 * Note the following EC Values (in bits [31:26]
+	 * 0x00 (d 0): unknown
+	 * 0x01 (d 1): Trapped WFI/WFE
+	 * 0x0E (d 14): Illegal execution state
+	 * 0x15 (d 21): SVC from AArch64 (aka syscall)
+	 * 0x20 (d 32): Instruction abort from lower EL
+	 * 0x24 (d 36): Data abort from lower EL
+	 * 0x25 (d 37): Data abort from same EL
+	 * 0x2C (d 44): Stack pointer alignment fault
+	 * 0x3C (d 60): BRK instruction (aka debug breakpoint)
+	 */
+
+}
+
 // Core0 timer interrupt source is at 0x40000...60 on bit 1
 extern "C" void interrupt_init() {
 	// Disable all
