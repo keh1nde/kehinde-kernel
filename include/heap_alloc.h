2d3ffdbccb7abdc23d6f912e5d48e5945c365709
Kehinde Adeoso <kennolite@gmail.com>
2026-05-01 17:13:53 -0400

    Completed heap alloc methods

--- include/heap_alloc.h
+++ include/heap_alloc.h
@@ -5,4 +5,25 @@
 #ifndef RASPBERRY_PI_OPERATING_SYSTEM_HEAP_ALLOC_H
 #define RASPBERRY_PI_OPERATING_SYSTEM_HEAP_ALLOC_H
 
+#include <stdint-gcc.h>
+#include <stdint.h>
+
+
+#include "pmm.h"
+#include "mmu.h"
+#include "uart.h"
+
+
+constexpr uint64_t HEAP_BASE = 0x100000000; // Was originally 0x100000000ULL
+constexpr uint64_t HEAP_MAX_SIZE = 0x100000;
+constexpr uint64_t HEAP_ALIGN = 16;
+inline uint64_t bump_ptr;
+inline uint64_t heap_end;
+
+void kheap_init();
+
+void* kmalloc(uint64_t size);
+
+[[noreturn]] void panic(const char* msg);
+
 #endif //RASPBERRY_PI_OPERATING_SYSTEM_HEAP_ALLOC_H
