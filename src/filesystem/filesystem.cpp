277f81bf129978bfbcd0be19dd1259f670d5e516
Kehinde Adeoso <kennolite@gmail.com>
2026-05-13 02:15:07 -0400

    Completed fs_create implementation

--- src/filesystem/filesystem.cpp
+++ src/filesystem/filesystem.cpp
@@ -57,7 +57,139 @@ uint64_t fs_lookup(const char* path) {
 	return curr->ino;
 }
 
-uint64_t fs_create(uint64_t parent_ino, const char* name, uint8_t type) {
+uint64_t fs_create(uint64_t parent_ino, const char* name, const uint8_t type) {
+	inode *parent = find_inode(parent_ino);
+	if (!parent) return INVALID_INO;
+	if (parent->inode_type != 2) return INVALID_INO; // The inode isn't a directory.
+
+	// Validate name
+	if (__builtin_strlen(name) >= MAX_SIZE || name[0] == '\0') return INVALID_INO;
+
+	// Check if name exists in the directory
+	const uint64_t dirents_per_block = sizeof(block::data) / sizeof(dirent);
+
+	for (int i = 0; i < parent->inode_size; i++) {
+		const dirent* d = dirent_at(parent, i);
+		// Check if d->name matches, if so, return invalid
+		if (!d) return INVALID_INO;
+		if (name_matches_single(d->name, name)) return INVALID_INO;
+	}
+
+	// Allocate and initialize a new inode
+	inode* new_inode = static_cast<inode *>(kmalloc(sizeof(inode)));
+	new_inode->ino = next_ino++;
+	new_inode->inode_type = type;
+	new_inode->inode_size = 0;
+	new_inode->first_block = nullptr;
+	new_inode->next = inode_list_head;
+	inode_list_head = new_inode;
+
+	// Insert new dirent into parent inode.
+	if (parent->inode_size % dirents_per_block == 0) {
+		append_block(parent);
+	}
+
+	// Insert dirent into parent inode
+	const uint64_t block_index = parent->inode_size / dirents_per_block;
+	const uint64_t slot = parent->inode_size % dirents_per_block;
+
+	block_t* curr_block = parent->first_block;
+
+	for (uint64_t j = 0; j < block_index; j++) {
+		if (curr_block) curr_block = curr_block->next;
+		else break; // dir itself is null, so there's no next
+	}
+
+	// Write a directory entry in the parent's block containing information
+	parent->inode_size++;
+	dirent *new_dirent = &reinterpret_cast<dirent*>(curr_block->data)[slot];
+	new_dirent->inode_id = new_inode->ino;
+
+	int i = 0;
+	while (name[i] != '\0') {
+		new_dirent->name[i] = name[i];
+		i++;
+	}
+	new_dirent->name[i] = '\0';
+
+	return new_inode->ino;
+}
+
+int fs_unlink(const uint64_t parent_ino, const char *name) {
+	// Get parent. If parent does not exist or is not a directory, return.
+	inode* parent = find_inode(parent_ino);
+	if (!parent) return -1; // inode with matching number not found
+	if (parent->inode_type != 2 || parent->inode_size == 0) return -1; // not a directory, or the directory is empty
+
+	// Find a dirent with a matching name. If not found return -1
+	dirent* subject_dirent = nullptr;
+	for (int i = 0; i < parent->inode_size; i++) {
+		subject_dirent = dirent_at(parent, i);
+		if (!subject_dirent) return -1;
+		if (name_matches_single(subject_dirent->name, name)) break;
+	}
+
+	// If we exit the loop without a match, or if subject_dirent remains null.
+	if (!subject_dirent || !name_matches_single(subject_dirent->name, name)) return -1;
+
+	// Swap the dirent-to-be-deleted with the entry it points to
+	dirent* last_dirent = dirent_at(parent, parent->inode_size - 1);
+	if (!last_dirent) return -1;
+
+	// Fail if the inode to be deleted is a non-empty directory.
+	inode* subject_inode = find_inode(subject_dirent->inode_id);
+	if (!subject_inode || (subject_inode->inode_type == 2 && subject_inode->inode_size != 0)) return -1;
+
+	if (subject_dirent == last_dirent) {
+		parent->inode_size--;
+		return 0;
+	}
+
+	*subject_dirent = *last_dirent;
+	parent->inode_size--;
+	return 0;
+}
+
+int64_t fs_read(const uint64_t ino, const uint64_t offset, uint64_t len, void *buf) {
+	uint8_t* dst = static_cast<uint8_t *>(buf);
+
+	if (!buf) return -1;
+
+	const uint64_t block_index = offset / sizeof(block::data);
+	const uint64_t byte_offset = offset % sizeof(block::data);
+
+	const inode* subject = find_inode(ino);
+	if (!subject) return -1; // The inode does not exist
+	if (subject->inode_type != 1) return -1; // inode doesn't represent a file
+
+	const block_t* current_block = subject->first_block;
+
+
+	for (int i = 0; i < block_index; i++) {
+		if (current_block) current_block = current_block->next;
+		else return -1; // There are no blocks allocated, so there's nothing to read.
+	}
+
+	if (offset >= subject->inode_size) return 0;
+
+	if (offset + len > subject->inode_size) {
+		len = subject->inode_size - offset;
+	}
+
+
+	uint64_t bytes_copied = 0;
+	uint64_t block_offset = byte_offset;
+
+	while (bytes_copied < len) {
+		const uint64_t bytes_left_in_block = sizeof(block::data) - block_offset;
+		const uint64_t chunk = min(len - bytes_copied, bytes_left_in_block);
+
+		for (int i = 0; i < chunk; i++) {
+			dst[bytes_copied + i] = current_block->data[block_offset + i];
+		}
+
+		bytes_copied += chunk;
+		block_offset = 0;
 
 }
 
