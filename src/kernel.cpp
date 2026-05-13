1b40574c725949d2255ecdd8a6df58659e2d7beb
Kehinde Adeoso <kennolite@gmail.com>
2026-05-13 02:20:46 -0400

    Removed test code from kernel_main

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -16,27 +16,6 @@ extern "C" void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
 	(void) r1;
 	(void) atags;
 
-	uart_init();
-
-	uart_puts("Today is the ");
-	uart_put_uint(29);
-	uart_puts(" of April. \n");
-
-	uint64_t el;
-	asm volatile("mrs %0, CurrentEL" : "=r"(el));
-	switch (el) {
-		case (4):
-			uart_puts("This is Exception Level One \n");
-			break;
-		case (8):
-			uart_puts("This is Exception Level Two \n");
-			break;
-		case (12):
-			uart_puts("This is Exception Level Three \n");
-		default: uart_puts("No EL found \n");
-	}
-
-
 
 	//pmm_init();
 	// uart_puts("Page Frame Allocator initialized.\n");
@@ -48,16 +27,9 @@ extern "C" void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
 	kheap_init();
 	uart_puts("Heap allocator initialized.\n");
 
-	// Testing kmalloc
-	void* p = kmalloc(64);
-	uart_puts("kmalloc was called and returned the following address: ");
-	uart_put_hex(reinterpret_cast<uint64_t>(p));
-	uart_puts("\n");
-
-	// Testing if memory is usable
-	uint64_t* p1 = static_cast<uint64_t*>(kmalloc(64));
-	p1[0] = 0xDEADBEEFCAFEBABE;
-	p1[1] = 0x1234567890ABCDEF;
+	// ---- Filesystem tests ----
+	fs_init();
+	uart_puts("fs_init done.\n");
 
 	if (p1[0] == 0xDEADBEEFCAFEBABE && p1[1] == 0x1234567890ABCDEF) {
 		uart_puts("kmalloc R/W ok\n");
